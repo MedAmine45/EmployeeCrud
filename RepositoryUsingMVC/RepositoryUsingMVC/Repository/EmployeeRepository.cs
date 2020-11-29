cc2040231118283b695d37ca5fc7c21ee6d1311a
Mohamed Amine Ben Noureddine <mohamed.bennoureddine@outlook.com>
2020-11-29 19:44:40 +0100

    initial commit

---
+++ RepositoryUsingMVC/RepositoryUsingMVC/Repository/EmployeeRepository.cs
@@ -0,0 +1,71 @@
+﻿using RepositoryUsingMVC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryUsingMVC.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDBContext _context;
+        public EmployeeRepository()
+        {
+            _context = new EmployeeDBContext();
+        }
+        public EmployeeRepository(EmployeeDBContext context)
+        {
+            _context = context;
+        }
+     
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee GetById(int EmployeeID)
+        {
+            return _context.Employees.Find(EmployeeID);
+        }
+
+        public void Insert(Employee employee)
+        {
+            _context.Employees.Add(employee);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Employee employee)
+        {
+            _context.Entry(employee).State = EntityState.Modified;
+        }
+        public void Delete(int EmployeeID)
+        {
+            Employee employee = _context.Employees.Find(EmployeeID);
+            _context.Employees.Remove(employee);
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ RepositoryUsingMVC/RepositoryUsingMVC/Repository/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using RepositoryUsingMVC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryUsingMVC.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
